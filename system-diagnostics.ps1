e4d6e5b8eb282268c3be24f04d0d6f81b109f4e4
Julentry <julien.durel@protonmail.com>
2026-03-04 09:29:11 +0000

    Update system-diagnostics.ps1

--- system-diagnostics.ps1
+++ system-diagnostics.ps1
@@ -1,11 +1,33 @@
+Write-Host "============================="
 Write-Host "SYSTEM INFORMATION"
-Get-ComputerInfo
+Write-Host "============================="
 
-Write-Host "RUNNING PROCESSES"
-Get-Process
+Get-ComputerInfo |
+Select-Object WindowsProductName, WindowsVersion, CsName, CsProcessors, CsTotalPhysicalMemory
 
-Write-Host "SERVICES STATUS"
-Get-Service
+Write-Host ""
+Write-Host "============================="
+Write-Host "TOP RUNNING PROCESSES"
+Write-Host "============================="
 
+Get-Process |
+Sort-Object CPU -Descending |
+Select-Object -First 10 Name, CPU
+
+Write-Host ""
+Write-Host "============================="
+Write-Host "SERVICES (Stopped but set to Auto)"
+Write-Host "============================="
+
+Get-Service |
+Where-Object { $_.Status -eq 'Stopped' -and $_.StartType -eq 'Automatic' } |
+Sort-Object DisplayName |
+Select-Object -First 25 Status, StartType, Name, DisplayName
+
+Write-Host ""
+Write-Host "============================="
 Write-Host "DISK SPACE"
-Get-PSDrive
+Write-Host "============================="
+
+Get-PSDrive -PSProvider FileSystem |
+Select-Object Name, Used, Free
